da688b53c7b0e388a1ea7ba8ecf7520dfa874f52
reckigit <frandes.daring@gmail.com>
2021-01-06 21:12:20 +0700

    modified: dictionary.py

--- variable-types/dictionary.py
+++ variable-types/dictionary.py
@@ -1,15 +1,15 @@
 # ADA DUA DICTIONARY
-nomor = {}
-nomor['satu'] = 'Ini adalah 1'
-nomor[2] = 'Ini adalah 2'
+itsnum = {}
+itsnum['satu'] = 'Ini adalah 1'
+itsnum[2] = 'Ini adalah 2'
 itsme = {'myname':'Recki','myjob':'coding','myhobby':'menulis'}
 
-# CETAK LENGKAP DICTIONARY nomor DI LAYAR DI LAYAR
-print(nomor)
-# CETAK DICTIONARY nomor MENGGUNAKAN KEY satu DI LAYAR
-print(nomor['satu'])
-# CETAK DICTIONARY nomor MENGGUNAKAN KEY 2 DI LAYAR
-print(nomor[2])
+# CETAK LENGKAP DICTIONARY itsnum DI LAYAR DI LAYAR
+print(itsnum)
+# CETAK DICTIONARY itsnum MENGGUNAKAN KEY satu DI LAYAR
+print(itsnum['satu'])
+# CETAK DICTIONARY itsnum MENGGUNAKAN KEY 2 DI LAYAR
+print(itsnum[2])
 # CETAK LENGKAP DICTIONARY itsme DI LAYAR
 print(itsme)
 # CETAK DICTIONARY itsme MENGGUNAKAN KEY myname DI LAYAR
