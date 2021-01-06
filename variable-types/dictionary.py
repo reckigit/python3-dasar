087bac71d08827e9543f6925a893742f36cf5748
reckigit <frandes.daring@gmail.com>
2021-01-06 21:08:47 +0700

    modified: variable-types/*

---
+++ variable-types/dictionary.py
@@ -0,0 +1,24 @@
+# ADA DUA DICTIONARY
+nomor = {}
+nomor['satu'] = 'Ini adalah 1'
+nomor[2] = 'Ini adalah 2'
+itsme = {'myname':'Recki','myjob':'coding','myhobby':'menulis'}
+
+# CETAK LENGKAP DICTIONARY nomor DI LAYAR DI LAYAR
+print(nomor)
+# CETAK DICTIONARY nomor MENGGUNAKAN KEY satu DI LAYAR
+print(nomor['satu'])
+# CETAK DICTIONARY nomor MENGGUNAKAN KEY 2 DI LAYAR
+print(nomor[2])
+# CETAK LENGKAP DICTIONARY itsme DI LAYAR
+print(itsme)
+# CETAK DICTIONARY itsme MENGGUNAKAN KEY myname DI LAYAR
+print(itsme['myname'])
+# CETAK DICTIONARY itsme MENGGUNAKAN KEY myjob DI LAYAR
+print(itsme['myjob'])
+# CETAK DICTIONARY itsme MENGGUNAKAN KEY myhobby DI LAYAR
+print(itsme['myhobby'])
+# CETAK KEYS DICTIONARY DI LAYAR
+print(itsme.keys())
+# CETAK VALUES DICTIONARY DI LAYAR
+print(itsme.values())
