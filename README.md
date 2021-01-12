bc4ffbfbfd888e4da2692d07444ef264a32bc575
reckigit <frandes.daring@gmail.com>
2021-01-12 11:46:59 +0700

    modified: README.md home.py

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # python3-dasar
+Belajar kode atau pemograman python3 dari dasar.
+
+## Tentang kode ini
+Sumber kode sengaja dibuat oleh penulis untuk,
+keperluan belajar bagi pemula python.
+Kode ditulis dengan skrip python3
+Python merupakan bahasa pemograman lintas platform,
+mudah dipahami dan dipraktikkan bagi pemula.
+
+## Apa yang baru
+Terus dalam pengembangan (penulisan).
+
+## Petunjuk penggunaan
+* Instalasi Python3
+** `sudo apt install python3`
+* Instalasi Git
+** `sudo apt install git`
+** `git clone https://github.com/reckigit/python3-dasar`
+* Menjalankan program
+** `python3 home.py`
--- home.py
+++ home.py
@@ -16,47 +16,74 @@ def head():
     print("="*22)
 
 # FUNGSI INPUT PROMPT MENU
-def promp():
+def opt():
     global menuselect
-    try:
-        menuselect = int(input("\nPilih Menu No. > "))
-    except ValueError:
-        print("\nAnda belum memilih,")
-        continu = input("Apakah anda masih ingin melanjutkan program ? (Y/y)\n> ")
-        if (continu.upper() == 'Y'):
-            promp()
-        else:
-            exit()
+    while True:
+        try:
+            menuselect = int(input("\nPilih Menu No. > "))
+            break
+        except ValueError:
+            print("\nAnda belum memilih,")
+            continu = input("Apakah anda masih ingin melanjutkan program ? (Y/y)\n> ")
+            if (continu.upper() == 'Y'):
+                opt()
+            else:
+                exit()
 
 # FUNGSI CETAK MENU UTAMA DI LAYAR
 def mainmenu():
     for Key in MainMenu:
         for Menu in MainMenu[Key]:
             print(f"{Key}. {Menu}")
-    promp()
+    opt()
+    global menu1selected
+    menu1selected = menuselect
     gosubmenu()
     
 # FUNGSI CETAK SUBMENU DI LAYAR
 def gosubmenu():
     os.system('clear')
-    for Menu in MainMenu[menuselect]:
-        print(f"{Menu}, terdapat pilihan :")
-        for SubMenu in MainMenu[menuselect][Menu]:
-            print(f"{SubMenu}. {MainMenu[menuselect][Menu][SubMenu]}")
-    promp()
-    global menu,selected
-    menu = Menu
-    selected = menuselect
-    reading()
+    try:
+        for Menu in MainMenu[menuselect]:
+            print(f"{Menu}, terdapat pilihan :")
+            for SubMenu in MainMenu[menuselect][Menu]:
+                print(f"{SubMenu}. {MainMenu[menuselect][Menu][SubMenu]}")
+        opt()
+        global menu
+        menu = Menu
+        reading()
+    except KeyError:
+        begin()
     
 def reading():
     os.system('clear')
-    container = (MainMenu[selected]).lower()).strip(" ","-")
-    filereading = f"{(MainMenu[selected][menu][menuselect]).lower()}.py"
-    print(filereading)
-    print('•'*48)
-    os.system(f"cat {container}/{filereading}")
-    print('•'*48)
+    container = menu.lower().replace(" ","-")
+    # ignore•me -- print(container,menu1selected,menu,menuselect)
+    try:
+        filereading = f"{(MainMenu[menu1selected][menu][menuselect]).lower()}.py"
+        print(f"Sedang membaca file : '{filereading}'")
+        print('•'*48)
+        os.system(f"cat {container}/{filereading}")
+        print('•'*48)
+        dorunning = input("\nIngin menjalankan skrip tersebut ? (Y/y)\n> ")
+        if (dorunning.upper() == 'Y'):
+            running()
+        else:
+            pass
+    except KeyError:
+        begin()
+
+def running():
+    container = menu.lower().replace(" ","-")
+    # ignore•me -- print(container,menu1selected,menu,menuselect)
+    try:
+        filereading = f"{(MainMenu[menu1selected][menu][menuselect]).lower()}.py"
+        print(f"\nBerikut hasil menjalankan '{filereading}'")
+        print('•'*48)
+        os.system(f"python3 {container}/{filereading}")
+        print('•'*48)
+    except KeyError:
+        begin()    
 
 # FUNGSI MULAI PROGRAM
 def begin():
